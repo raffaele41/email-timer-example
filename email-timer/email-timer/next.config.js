24f202f9b5261e75def1e42d55ed7afe10474c84
raffaele41 <60689722+raffaele41@users.noreply.github.com>
2026-03-10 17:22:53 +0100

    Update next.config.js

--- email-timer/email-timer/next.config.js
+++ email-timer/email-timer/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
     outputFileTracingIncludes: {
-      './app/api/countdown/route': ['./email-timer/email-timer/*.ttf'],
+      './app/api/countdown/route': ['./*.ttf'],
     },
   },
   webpack: (config, { isServer }) => {
