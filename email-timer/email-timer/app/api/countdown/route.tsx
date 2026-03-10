748279c929cd44daf606373684230187f8ffc4ed
raffaele41 <60689722+raffaele41@users.noreply.github.com>
2026-03-10 17:30:06 +0100

    Update route.tsx

--- email-timer/email-timer/app/api/countdown/route.tsx
+++ email-timer/email-timer/app/api/countdown/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 export const runtime = 'nodejs' 
+export const dynamic = 'force-dynamic'
 export async function GET(req: NextRequest) {
   try {
     const GIFEncoder = (await import('gifencoder')).default
